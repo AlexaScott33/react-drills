6265edc0ca565c7ea6d8b26e9f45ef7d5119d677
Alexa Scott <arscott4792@gmail.com>
2018-09-01 06:23:57 -0700

    completed drill

--- surprise/src/components/surprise-button.js
+++ surprise/src/components/surprise-button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function SurpriseButton(props) {
-    console.log(props.onClick);
+    console.log(props);
     return <button onClick={props.onClick}>Surprise!</button>;
 }
 
